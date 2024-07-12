729e7d5afdc031063e495ce6ac994fc9fb4e78d7
Tyrael <github@wkz.io>
2024-07-12 11:22:36 +0800

    1.0.15

---
+++ Casks/font-sarasa-mono-sc-ttf.rb
@@ -0,0 +1,21 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "font-sarasa-mono-sc-ttf" do
+  version "1.0.15"
+  desc "Sarasa Gothic / 更纱黑体 / 更紗黑體 / 更紗ゴシック / 사라사 고딕"
+  homepage ""
+  url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/SarasaMonoSC-TTF-#{version}.7z"
+  sha256 "998628009cb0001eb636f53c03abc1dbd89450e8391d09aeaf5e4cf5f10cb61c"
+
+  font "SarasaMonoSC-Bold.ttf"
+  font "SarasaMonoSC-BoldItalic.ttf"
+  font "SarasaMonoSC-ExtraLight.ttf"
+  font "SarasaMonoSC-ExtraLightItalic.ttf"
+  font "SarasaMonoSC-Italic.ttf"
+  font "SarasaMonoSC-Light.ttf"
+  font "SarasaMonoSC-LightItalic.ttf"
+  font "SarasaMonoSC-Regular.ttf"
+  font "SarasaMonoSC-SemiBold.ttf"
+  font "SarasaMonoSC-SemiBoldItalic.ttf"
+end
